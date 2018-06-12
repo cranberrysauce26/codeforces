3d2107875b6f9a2d28a6ae6d1f7a955635582b85
Steven <stevenmai132@yahoo.ca>
2018-06-12 00:21:48 -0400

    added a template folder

---
+++ template/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define LOCAL
+#ifdef LOCAL
+#define DEBUG(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else
+#define DEBUG(fmt, ...)
+#endif
+
+using namespace std;
+#define szof(v) ((int)(v).size())
+#define allof(v) begin(v), end(v)
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> pll;
+
+int main() {
+#ifdef LOCAL
+    freopen("data.txt", "r", stdin);
+#endif
+}
